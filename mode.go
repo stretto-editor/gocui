7c427be64bc13938378340a0286ea50009fd82a2
Valentin Matton <valentin.matton@ensimag.grenoble-inp.fr>
2016-05-22 17:55:40 +0200

    Added name Mode accessor

--- mode.go
+++ mode.go
@@ -5,12 +5,14 @@ type Mode struct {
 	keybindings kbSet
 }
 
-// CreateMode makes a new kbSet for the mode call name
-// If the mode already exists, the kbSet will be flush
 func CreateMode(name string) *Mode {
 	return &Mode{name: name}
 }
 
 func (m *Mode) GetKeyBindings() *kbSet {
 	return &m.keybindings
+}
+
+func (m *Mode) Name() string {
+	return m.name
 }
