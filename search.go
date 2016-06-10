9c0f9c52beb77bd54385d19854531d7491568d9e
Jean-Baptiste Boric <jblbeurope@gmail.com>
2016-06-10 21:51:35 +0200

    Cache and fix forward string search
    Searching a string immediately after a match was found would
    immediately find the very same match and not advance to the next one.
    Also, the previously searched string wasn't cached for subsequent
    invocations.

--- search.go
+++ search.go
@@ -4,9 +4,18 @@ import "strings"
 
 func (v *View) SearchForward(pattern string) (bool, int, int) {
 	rx, ry, _ := v.realPosition(v.cx, v.cy)
+	if len(pattern) == 0 {
+		pattern = v.searchString
+	}
 	if len(pattern) > 0 {
-		if ind := strings.Index(string(v.lines[ry][rx:]), pattern); ind > -1 {
-			return true, ind + rx, ry
+		v.searchString = pattern
+
+		// Start searching one character beyond where we are
+		// or we won't be able to continue to the next match
+		if len(v.lines[ry]) > rx+1 {
+			if ind := strings.Index(string(v.lines[ry][rx+1:]), pattern); ind > -1 {
+				return true, ind + rx, ry
+			}
 		}
 		for i := ry + 1; i < len(v.lines); i++ {
 			if ind := strings.Index(string(v.lines[i]), pattern); ind > -1 {
